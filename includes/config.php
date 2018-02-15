e9ee6f61ef6668029fcec42e6270a47f31afdc18
ABDULGAFAAR ABIOYE <32104160+Abdulgafaar@users.noreply.github.com>
2018-02-15 07:07:35 +0100

    DB Constants

---
+++ includes/config.php
@@ -0,0 +1,10 @@
+<?php
+/* database constants
+if (!definded('DB_SERVER')) {define("DB_SERVER", "localhost")
+}
+*/
+definded('DB_SERVER') ? null : define("DB_SERVER", "localhost");
+definded('DB_USER') ? null : define("DB_USER", "gallery");
+definded('DB_PASS') ? null : define("DB_PASS", "guguru");
+definded('DB_NAME') ? null : define("DB_NAME", "photo_gallery");
+?>
