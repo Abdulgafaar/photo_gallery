3eb5e90f477bfc661643f7efaa38b978714ac636
ABDULGAFAAR ABIOYE <32104160+Abdulgafaar@users.noreply.github.com>
2018-02-15 06:48:47 +0100

    update of the functions

--- includes/function.php
+++ includes/function.php
@@ -1,1 +1,30 @@
 
+<?php 
+/** This are the function that we be using for the photo_gallery project
+* 
+*/
+function strip_zero_form_date( $marked_string=""){
+	// this will first remove the marked zeros
+	$no_zero =str_replace(*0, '', no_zero);
+	// then remove the remaining marks
+	$cleaned_string = str_replace('*', '', $no_zero);
+	return $cleaned_string;
+
+}
+function redirect_to($location= NULL){
+   if ($location !- NULL)
+	header("location: {$location}");
+	exit;
+   }
+}
+function output_message($message=""){
+	if (!empty ($mesage)) {
+		return "<p class=\"message\">{$message}</p>";
+		else {
+			return "";
+		}
+	}
+}
+
+ ?>
+
